86c32ae74650808ee1ca3de37cad8b0a7a9e0739
Chase Moskal <chasemoskal@gmail.com>
2025-04-12 14:06:04 -0700

    improve: deadline

--- s/deadline.ts
+++ s/deadline.ts
@@ -1,9 +1,17 @@
 
+export class DeadlineError extends Error {
+	name = this.constructor.name
+	constructor(milliseconds: number) {
+		super(`deadline exceeded (${milliseconds.toLocaleString()} ms)`)
+	}
+}
+
+/** set a deadline for a fn to do something, will reject with a `DeadlineError` if it takes too long */
 export function deadline<R>(milliseconds: number, fn: () => Promise<R>) {
 	return new Promise<R>((resolve, reject) => {
 
 		const id = setTimeout(
-			() => reject(new Error("timeout error")),
+			() => reject(new DeadlineError(milliseconds)),
 			milliseconds,
 		)
 
