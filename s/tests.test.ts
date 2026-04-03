9f1f0fd5f1f03809334faac907629f324d3dd2bf
Chase Moskal <chasemoskal@gmail.com>
2026-04-03 00:15:36 -0700

    tweak: test index

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,5 +1,5 @@
 
-import {Science} from "@e280/science"
+import {science} from "@e280/science"
 
 import data from "./data/data.test.js"
 import debounce from "./debounce/debounce.test.js"
@@ -11,7 +11,7 @@ import maybe from "./maybe/test.js"
 import dig from "./dig/test.js"
 import ok from "./ok/test.js"
 
-await Science.run({
+await science.run({
 	data,
 	debounce,
 	microbounce,
