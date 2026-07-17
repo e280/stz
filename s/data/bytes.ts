e82f412c9939ca548863e33c113a89e7826a9c90
Chase Moskal <chasemoskal@gmail.com>
2026-07-16 23:16:05 -0700

    add: bytes.concat

--- s/data/bytes.ts
+++ s/data/bytes.ts
@@ -10,5 +10,20 @@ export const bytes = Object.freeze({
 	random(count: number) {
 		return crypto.getRandomValues(new Uint8Array(count))
 	},
+
+	concat(parts: Uint8Array[]) {
+		let size = 0
+		for (const part of parts) size += part.byteLength
+
+		const bytes = new Uint8Array(size)
+
+		let offset = 0
+		for (const part of parts) {
+			bytes.set(part, offset)
+			offset += part.byteLength
+		}
+
+		return bytes as Uint8Array
+	},
 })
 
