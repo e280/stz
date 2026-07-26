8e2d15b9623c28d10841106f147feb591e22362e
Chase Moskal <chasemoskal@gmail.com>
2026-07-25 19:44:22 -0700

    export: gotYay and gotNay directly

--- s/maybe/index.ts
+++ s/maybe/index.ts
@@ -1,5 +1,5 @@
 
 export * as maybe from "./index.barrel.js"
-export {yay, nay, validator, allow, deny} from "./index.barrel.js"
+export {yay, nay, gotYay, gotNay, validator, allow, deny} from "./index.barrel.js"
 export * from "./types.js"
 
