9ef84287a76222ce5d8eddcf6449f42d91b82a0c
Chase Moskal <chasemoskal@gmail.com>
2026-09-17 02:22:10 -0700

    improve: deadline can accept a promise

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,9 @@
 
 ## v0.3
 
+### v0.3.11
+- 🍏 allow the `deadline` fn to accept either a fn or a promise
+
 ### v0.3.10
 - 🍏 update deps
 
--- s/deadline.ts
+++ s/deadline.ts
@@ -1,4 +1,6 @@
 
+import {is} from "./is.js"
+
 export class DeadlineError extends Error {
 	name = this.constructor.name
 	constructor(public milliseconds: number) {
@@ -6,17 +8,23 @@ export class DeadlineError extends Error {
 	}
 }
 
+async function invoke<R>(fn: Promise<R> | (() => Promise<R>)) {
+	return is.fn(fn)
+		? fn()
+		: fn
+}
+
 /** set a deadline for a fn to do something, will reject with a `DeadlineError` if it takes too long */
-export function deadline<R>(milliseconds: number, fn: () => Promise<R>) {
+export function deadline<R>(milliseconds: number, fn: Promise<R> | (() => Promise<R>)) {
 	if (milliseconds <= 0 || milliseconds === Infinity)
-		return fn()
+		return invoke(fn)
 
 	return new Promise<R>((resolve, reject) => {
 		const id = setTimeout(
 			() => reject(new DeadlineError(milliseconds)),
 			milliseconds,
 		)
-		fn()
+		invoke(fn)
 			.then(resolve)
 			.catch(reject)
 			.finally(() => clearTimeout(id))
